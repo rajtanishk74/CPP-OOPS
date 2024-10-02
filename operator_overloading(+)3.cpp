c60d98011f1cee5e60aaaa39b89783d578cb1317
rajtanishk74 <154700300+rajtanishk74@users.noreply.github.com>
2024-10-03 00:38:18 +0530

    operator(+)overloading3

---
+++ operator_overloading(+)3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+   class Complex
+   {
+       int real,img;
+       public:
+       Complex(){
+           
+       };
+       Complex(int real ,int img)
+       {
+          this->real=real;
+          this->img=img;
+       };
+       void display()
+       {
+           cout<<real<<"+"<<img<<endl;
+       }
+       Complex operator +(Complex &c)
+       {
+          Complex ans;
+          ans.real=real+c.real;
+          ans.img=img+c.img;
+          return ans;
+       }
+   };
+int main() {
+    Complex c1(3,4);
+    Complex c2(4,6);
+    Complex c3=c1+c2; //c1.fun(c2)
+    c3.display();
+    return 0;
+}
